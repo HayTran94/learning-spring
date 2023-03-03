49ebbe0163280741931b306d13418d772c528df0
haytv <haytv@vng.com.vn>
2023-03-03 22:42:23 +0700

    add cron

---
+++ src/main/java/com/example/accessingdatajpa/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package com.example.accessingdatajpa;
+
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+
+    @Scheduled(fixedDelay = 3000)
+    public void task() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
